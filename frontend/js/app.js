f03fad7059d68c0db11ec86e2015c4a51dc92fb5
Festac <fesjo2009@gmail.com>
2026-03-23 14:06:01 +0100

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -250,3 +250,18 @@ if (path.includes("dashboard")) {
   loadBalance();
   loadAdminButton();
 }
+
+// For Admin
+function loadAdminUI() {
+  const token = getToken();
+  if (!token) return;
+
+  const payload = JSON.parse(atob(token.split(".")[1]));
+
+  if (payload.role === "admin") {
+    document.getElementById("adminCard").style.display = "block";
+  }
+}
+
+// Call this after the page loads
+document.addEventListener("DOMContentLoaded", loadAdminUI);
